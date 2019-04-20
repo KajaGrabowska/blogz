25aefce59dbbb4dfdc50023f4c3ff1f59edafdeb
Kaja Grabowska <kaja.d.grabowska@gmail.com>
2019-04-19 22:20:12 -0500

    Blog posts appear on the page

---
+++ main.py
@@ -0,0 +1,52 @@
+from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:wakacje@localhost:3306/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+
+class Blog(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    body = db.Column(db.Text)
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+
+
+@app.route('/newpost', methods=['POST', 'GET'])
+def newpost():
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        new_post = Blog(title, body)
+        db.session.add(new_post)
+        db.session.commit()
+    
+
+@app.route('')
+
+    blog_posts = Blog.query.all()
+    #completed_tasks = Task.query.filter_by(completed=True).all()
+    return render_template('blog.html', blog_posts = blog_posts)
+
+
+@app.route('/delete-task', methods=['POST'])
+def delete_task():
+
+    task_id = int(request.form['task-id'])
+    task = Task.query.get(task_id)
+    task.completed = True
+    db.session.add(task)
+    db.session.commit()
+
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    app.run()
